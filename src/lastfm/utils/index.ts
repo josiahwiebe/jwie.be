83f45ac5c9751382c0026a63da1074837841831d
josiahwiebe <josiah@collectif.co>
2025-12-22 13:29:01 -0600

    chore: remove deprecated files and configurations

--- src/lastfm/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export const formatNumber = (num: number) => {
-  return new Intl.NumberFormat('en-CA', { maximumFractionDigits: 2 }).format(Math.round(num * 100) / 100)
-}
--- templates/404.php
+++
@@ -1,9 +0,0 @@
-<?php ?>
-
-<section class='page-content grid items-center'>
-  <div class='flex flex-col gap-4'>
-    <h1 class='text-2xl font-heading font-bold sm:text-3xl md:text-4xl dark:text-slate-300'>
-      404 - Page Not Found
-    </h1>
-  </div>
-</section>
